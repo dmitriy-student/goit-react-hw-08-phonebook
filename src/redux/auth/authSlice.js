a870dc084d16a04ca85ddcb7d62b652cabd3bf64
dmitriy-student <dima.ermol22@gmail.com>
2023-08-06 23:23:25 +0300

    add files

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,15 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {},
+});
+
+export default authSlice.reducer;
