fc6e2e9d044cfc9deaf95593aee43f2fb04a0698
dmitriy-student <dima.ermol22@gmail.com>
2023-08-14 17:59:09 +0300

    Update FormPhonebook.jsx

--- src/components/FormPhonebook/FormPhonebook.jsx
+++ src/components/FormPhonebook/FormPhonebook.jsx
@@ -48,7 +48,7 @@ export default function FormPhonebook({ formSubmit }) {
           type="text"
           name="name"
           value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Zа-яА-Я\s]+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           onChange={handleInput}
           required
@@ -61,7 +61,7 @@ export default function FormPhonebook({ formSubmit }) {
           type="tel"
           name="number"
           value={number}
-          pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+          pattern="\+?[0-9\s\-\(\)]+"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           onChange={handleInput}
           required
